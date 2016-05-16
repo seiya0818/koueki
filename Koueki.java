abe04a7499519b310a6b630ec77eebbfeb4a4f0e
seiya0818 <seiya_minecraft@yahoo.co.jp>
2016-05-17 00:05:12 +0900

    Add files via upload

--- Koueki.java
+++ Koueki.java
@@ -1,21 +1,32 @@
 package jp.seiya0818;
 
-
 import org.bukkit.ChatColor;
-import org.bukkit.event.Listener;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Koueki extends JavaPlugin implements Listener
+public class Koueki extends JavaPlugin
 {
+	private KouekiCommandExecutor command;
+	public static String LoggerPrefix = ChatColor.WHITE + "[" + ChatColor.RED + "koueki_plugin" + ChatColor.WHITE + "]";
+	public static String PlayerPrefix = ChatColor.WHITE + "[" + ChatColor.RED + "交易" + ChatColor.WHITE + "]";
+	
 	public void onEnable()
 	{
 		//プラグイン読み込み時の処理
-		System.out.println(ChatColor.GREEN + "Koueki Plugin正常に読み込まれました。");
+		getLogger().info(LoggerPrefix + ChatColor.GREEN + "Koueki Plugin正常に読み込まれました。");
+		command = new KouekiCommandExecutor();
 	}
 	
 	public void onDisable()
 	{
 		//プラグイン終了時の処理
-		System.out.println(ChatColor.GREEN + "Koueki Pluginが正常に終了しました。");
+		getLogger().info(LoggerPrefix + ChatColor.GREEN + "Koueki Pluginが正常に終了しました。");
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		return this.command.onCommand(sender, cmd, label, args);
 	}
 }
+
--- KouekiCommandExecutor.java
+++ KouekiCommandExecutor.java
@@ -15,39 +15,46 @@ import org.bukkit.inventory.ItemStack;
 public class KouekiCommandExecutor implements CommandExecutor
 {
 
-	private Player player;
-	private Inventory tempInventory;
-	private Inventory tempArmors ;
-	private int tempLevel;
-	private float tempExp;
+	protected static Player player;
+	protected static Inventory tempInventory;
+	protected static Inventory tempArmors ;
+	protected static int tempLevel;
+	protected static float tempExp;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) 
 	{
 		switch(args.length)
 		{
+		case 0:
+			if(sender.hasPermission("koueki.help"))
+			{
+				Player player = (Player) sender;
+				PlayerCommands(player);
+				return true;
+			}
+			break;
 		case 1:
 			if(args[0].equalsIgnoreCase("start"))
 			{
 				//コンソールから実行不可に
 				if(!(sender instanceof Player))
 				{
-					sender.sendMessage(ChatColor.RED + "このコマンドはプレイヤーのみ使用可能です");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "このコマンドはプレイヤーのみ使用可能です");
 					return true;
 				}
 
 				//パーミッションチェック
 				if(!sender.hasPermission("koueki.start"))
 				{
-					sender.sendMessage(ChatColor.RED + "権限がありません");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "権限がありません");
 					return true;
 				}
 				if(sender.hasPermission("koueki.start"))
 				{
-					sender.sendMessage(ChatColor.GREEN + "交易モードを開始しました");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.GREEN + "交易モードを開始しました");
 					Player player = (Player) sender;
-					player.setGameMode(GameMode.ADVENTURE);
-					moveToTempInventory();
+					StartKoueki(player);
 					return true;
 				}
 			}
@@ -56,21 +63,29 @@ public class KouekiCommandExecutor implements CommandExecutor
 				//コンソールから(ry
 				if(!(sender instanceof Player))
 				{
-					sender.sendMessage(ChatColor.RED + "このコマンドはプレイヤーのみ使用可能です");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "このコマンドはプレイヤーのみ使用可能です");
 					return true;
 				}
 				//パーミッション(ry
 				if(!sender.hasPermission("koueki.quit"))
 				{
-					sender.sendMessage(ChatColor.RED + "権限がありません");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "権限がありません");
 					return true;
 				}
 				if(sender.hasPermission("koueki.quit"))
 				{
-					sender.sendMessage(ChatColor.GREEN + "交易モードを終了しました");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.GREEN + "交易モードを終了しました");
 					Player player = (Player) sender;
-					player.setGameMode(GameMode.SURVIVAL);
-					restoreInventory();
+					QuitKoueki(player);
+					return true;
+				}
+			}
+			else if(!args[0].equalsIgnoreCase("start") || !args[0].equalsIgnoreCase("quit"))
+			{
+				if(player.hasPermission("koueki.help"));
+				{
+					Player player = (Player) sender;
+					PlayerCommands(player);
 					return true;
 				}
 			}
@@ -81,58 +96,79 @@ public class KouekiCommandExecutor implements CommandExecutor
 			{
 				if(!(sender.hasPermission("koueki.start.others")))
 				{
-					sender.sendMessage(ChatColor.RED + "権限がありません");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "権限がありません");
 					return true;
 				}
 				if(sender.hasPermission("koueki.start.others"))
 				{
-					Player target = getPlayer(args[1]);
-					if(target == null)
+					Player player = getPlayer(args[1]);
+					if(player == null)
 					{
-						sender.sendMessage(ChatColor.RED + "プレイヤーはオフラインです。");
+						sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "プレイヤーはオフラインです。");
 						return true;
 					}
-					target.setGameMode(GameMode.ADVENTURE);
-					moveToTempInventory();
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.GREEN + args[1] + "の交易モードを開始させました");
+					StartKoueki(player);
 					return true;
 				}
 			}
-			if(args[0].equalsIgnoreCase("start"))
+			if(args[0].equalsIgnoreCase("quit"))
 			{
 				//パ(ry
-				if(!(sender.hasPermission("koueki.start.others")))
+				if(!(sender.hasPermission("koueki.quit.others")))
 				{
-					sender.sendMessage(ChatColor.RED + "権限がありません");
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "権限がありません");
 					return true;
 				}
-				if(sender.hasPermission("koueki.admin"))
+				if(sender.hasPermission("koueki.quit.others"))
 				{
-					Player target = getPlayer(args[1]);
-					if(target == null)
+					Player player = getPlayer(args[1]);
+					if(player == null)
 					{
-						sender.sendMessage(ChatColor.RED + "プレイヤーはオフラインです。");
+						sender.sendMessage(Koueki.PlayerPrefix + ChatColor.RED + "プレイヤーはオフラインです。");
 						return true;
 					}
-					target.setGameMode(GameMode.SURVIVAL);
-					restoreInventory();
+					sender.sendMessage(Koueki.PlayerPrefix + ChatColor.GREEN + args[1] + "の交易モードを終了させました");
+					QuitKoueki(player);
+					return true;
+				}
+			}
+			else if(!args[0].equalsIgnoreCase("start") || !args[0].equalsIgnoreCase("quit"))
+			{
+				if(sender.hasPermission("koueki.help"))
+				{
+					Player player = (Player) sender;
+					PlayerCommands(player);
 					return true;
 				}
 			}
 			break;
 		default:
-			sender.sendMessage(ChatColor.GOLD +  "======【/koueki start】=====");
-			sender.sendMessage(ChatColor.GOLD +  "====交易モードに設定します===");
-			sender.sendMessage(ChatColor.GOLD +  "======【/koueki quit】=====");
-			sender.sendMessage(ChatColor.GOLD +  "====交易モードを終了します===");
-			break;
+			if(sender.hasPermission("koueki.help"))
+			{
+				Player player = (Player) sender;
+				PlayerCommands(player);
+				return true;
+			}
 		}
 		return false;
 	}
+	
+	private void StartKoueki(Player player)
+	{
+		player.setGameMode(GameMode.ADVENTURE);
+		moveToTempInventory(player);
+	}
+	private void QuitKoueki(Player player)
+	{
+		player.setGameMode(GameMode.SURVIVAL);
+		restoreInventory(player);
+	}
 
-	private void moveToTempInventory()
+	private void moveToTempInventory(Player player)
 	{
 		//インベントリの保存
-		tempInventory = Bukkit.createInventory(player, 5*9);
+		tempInventory = Bukkit.createInventory(player, 5 * 9);
 		for(ItemStack item : player.getInventory().getContents())
 		{
 			if(item != null)
@@ -168,7 +204,7 @@ public class KouekiCommandExecutor implements CommandExecutor
 		player.setExp(0);
 	}
 	//インベントリ復帰
-    private void restoreInventory()
+    public void restoreInventory(Player player)
     {
 
         // インベントリの消去
@@ -216,5 +252,13 @@ public class KouekiCommandExecutor implements CommandExecutor
 	public static Player getPlayer(String name)
 	{
 		return Bukkit.getPlayerExact(name);
+	}
+	public void PlayerCommands(Player player)
+	{
+		player.sendMessage(ChatColor.MAGIC + "==========交易プラグイン==========");
+		player.sendMessage(ChatColor.GOLD +  "=========【/koueki start】=======");
+		player.sendMessage(ChatColor.GOLD +  "======交易モードに設定します=====");
+		player.sendMessage(ChatColor.GOLD +  "=========【/koueki quit】========");
+		player.sendMessage(ChatColor.GOLD +  "======交易モードを終了します=====");
 	}
 }
